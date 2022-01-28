1d8ddc030d98bf783f3a875bf2cb263fdbc4f49c
park <pjw4512@naver.com>
2022-01-28 15:29:53 +0900

    'v3.0'

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,19 +1,33 @@
-from flask import Blueprint, render_template, url_for , request
+import time
+
+from flask import Blueprint, render_template, url_for , request, session, jsonify, g
 from werkzeug.utils import redirect
 from werkzeug.utils import secure_filename
 import os
 from flask import Flask, request
-
+import pandas as pd
+import json
+from werkzeug.routing import BaseConverter, ValidationError
+from synthpop import Synthpop
+from warnings import simplefilter
+simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 bp = Blueprint('main', __name__, url_prefix='/')
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.debug = True
 
 from pybo.models import Question
+# from datasets.adult import df, dtypes
+# from pybo.synthpop.datasets.adult import df, dtypes
+
 
 @bp.route('/hello')
 def hello_pybo():
-    return 'Hello, Pybo!'
+    fff = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + g.user.username + ".csv")
+    return fff.to_html()
+
+
 
 @bp.route('/')
 def index():
@@ -24,13 +38,82 @@ def index():
 def render_file():
     return render_template('upload.html')
 
+global obj
+
 # 파일 업로드 처리
 @bp.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
+        # f = pd.DataFrame(data = request.files['file'])
         # 저장할 경로 + 파일명
-        f.save("./pybo/uploads/" + secure_filename(f.filename))
-        return render_template('upload2.html')
+
+        obj = g.user.username
+        #  ff = pd.DataFrame(data = f)
+        f.save("C:/finalproject/myproject/pybo/uploads/" + obj + '.csv')
+        # fff = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + '.csv', encoding='CP949')
+        fff = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + '.csv')
+        # return f.to_html()
+        df_info = fff
+        df_col = []
+        for i in range(0, len(df_info.columns)):
+            df_col.append(df_info.columns[i])
+    
+        
+        df2 = df_info.iloc[:10]
+
+        return render_template('upload2.html', df_list=df2, df_col = df_col, df_info=df_info)
+
+
+# json 생성
+@bp.route('/to_json', methods=['GET', 'POST'])
+def to_json():
+        obj = g.user.username
+        df_info = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + ".csv")
+        df_col = []
+
+        for i in range(0, len(df_info.columns)):
+            df_col.append(df_info.columns[i])
+        df_type = []
+        for i in range(0, len(df_info.columns)):
+            if (df_info.dtypes[i] == 'int64'):
+                df_type.append('int')
+            elif (df_info.dtypes[i] == 'float64'):
+                df_type.append('float')
+            else:
+                df_type.append('category')
+
+        to_json = {df_col[0]: df_type[0]}
+        # 이렇게 dict로 주지 않으면 list형식으로 들어감 ;
+        for i in range(1, len(df_info.columns)):
+            to_json[df_col[i]] = df_type[i]
+        with open("C:/finalproject/myproject/pybo/uploads/" + obj + ".json", 'w') as f:
+            json.dump(to_json, f)
+        
+        return render_template('to_json.html', df=df_info, df_col=df_col)
+    
+# json 생성
+@bp.route('/synth_generate', methods=['GET', 'POST'])
+def synth_generate():
+        obj = g.user.username
+
+        # df = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + ".csv")
+        with open("C:/finalproject/myproject/pybo/uploads/" + obj + ".json", 'r') as f:
+            dtypes = json.load(f)
+        columns = list(dtypes.keys())
+
+        df = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + ".csv", header=None, skiprows = 1, names=columns).astype(dtypes)
+        # 헤더가 있는 경우 -> skip
+        df.apply(pd.to_numeric, errors='coerce')
+
+        spop = Synthpop()
+        spop.fit(df, dtypes)
+
+        synth_df = spop.generate(len(df))
+        synth_df2 = synth_df.iloc[:10]
+        synth_df.to_csv("C:/finalproject/myproject/pybo/synth_dir/" + obj + ".csv")
+        return render_template('synth_generate.html', df=synth_df2, dtypes=synth_df.dtypes)
+        # return render_template('synth_generate.html', df=df, dtypes=dtypes)
+
 
 
