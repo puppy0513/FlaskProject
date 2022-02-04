143a9dfb9a9aaa216f86e3150afc7b82daa8417a
park <pjw4512@naver.com>
2022-02-04 17:06:30 +0900

    'v4.5'

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -8,6 +8,7 @@ from pybo.models import User
 
 import pandas as pd
 import json
+import functools
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -60,3 +61,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
+
