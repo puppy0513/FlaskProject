d93a659aa72da4d757e0046a412ad3ec3bb2d15a
park <pjw4512@naver.com>
2022-02-11 09:51:27 +0900

    'v5.1'

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -59,7 +59,7 @@ def render_file():
 
 
 
-# /home/ubuntu/projects/myproject/pybo/uploads
+# /home/ubuntu/FlaskProject/pybo/uploads
 # 파일 업로드 처리
 @bp.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
@@ -70,9 +70,9 @@ def upload_file():
 
         obj = g.user.username
         #  ff = pd.DataFrame(data = f)
-        f.save("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + '.csv')
+        f.save("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + '.csv')
         # fff = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + '.csv', encoding='CP949')
-        fff = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + '.csv')
+        fff = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + '.csv')
         # return f.to_html()
         df_info = fff
         df_col = []
@@ -89,7 +89,7 @@ def upload_file():
 @bp.route('/to_json', methods=['GET', 'POST'])
 def to_json():
         obj = g.user.username
-        df_info = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+        df_info = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
         df_col = []
 
         for i in range(0, len(df_info.columns)):
@@ -107,7 +107,7 @@ def to_json():
         # 이렇게 dict로 주지 않으면 list형식으로 들어감 ;
         for i in range(1, len(df_info.columns)):
             to_json[df_col[i]] = df_type[i]
-        with open("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json", 'w') as f:
+        with open("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json", 'w') as f:
             json.dump(to_json, f)
         
         return render_template('to_json.html', df=df_info, df_col=df_col)
@@ -117,7 +117,7 @@ def to_json():
 @bp.route('/to_json_part', methods=['GET', 'POST'])
 def to_json_part():
     obj = g.user.username
-    df_info = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+    df_info = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
     df_col = []
 
     for i in range(0, len(df_info.columns)):
@@ -135,7 +135,7 @@ def to_json_part():
     # 이렇게 dict로 주지 않으면 list형식으로 들어감 ;
     for i in range(1, len(df_info.columns)):
         to_json[df_col[i]] = df_type[i]
-    with open("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json", 'w') as f:
+    with open("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json", 'w') as f:
         json.dump(to_json, f)
 
     count = {}
@@ -148,7 +148,7 @@ def to_json_part():
 @bp.route('/to_json_part2', methods=['GET', 'POST'])
 def to_json_part2():
     obj = g.user.username
-    df_info = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+    df_info = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
     df_col = []
     list1 = []
     for i in range(0, len(df_info.columns)):
@@ -158,10 +158,10 @@ def to_json_part2():
             list1.append(request.form.get(df_col[i]))
     list2 = list(filter(None.__ne__, list1))
     df_info = df_info.drop(list2, axis=1)
-    df_info.to_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + '.csv')
-    df_info2 = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + '.csv')
+    df_info.to_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + '.csv')
+    df_info2 = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + '.csv')
     df_info2 = df_info2.iloc[:,1:]
-    df_info2.to_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + '.csv')
+    df_info2.to_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + '.csv')
     df_col2 = []
 
     for i in range(0, len(df_info2.columns)):
@@ -179,7 +179,7 @@ def to_json_part2():
     # 이렇게 dict로 주지 않으면 list형식으로 들어감 ;
     for i in range(1, len(df_info2.columns)):
         to_json[df_col2[i]] = df_type2[i]
-    with open("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json", 'w') as f:
+    with open("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json", 'w') as f:
         json.dump(to_json, f)
     count = {}
     for i in range(0, len(df_col2)):
@@ -194,11 +194,11 @@ def synth_generate():
         obj = g.user.username
 
         # df = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + ".csv")
-        with open("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json", 'r') as f:
+        with open("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json", 'r') as f:
             dtypes = json.load(f)
         columns = list(dtypes.keys())
 
-        df = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv", header=None, skiprows = 1, names=columns).astype(dtypes)
+        df = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv", header=None, skiprows = 1, names=columns).astype(dtypes)
         # 헤더가 있는 경우 -> skip
         df = df.iloc[:, 1:]
         df.apply(pd.to_numeric, errors='coerce')
@@ -208,7 +208,7 @@ def synth_generate():
 
         synth_df = spop.generate(len(df))
         synth_df2 = synth_df.iloc[:10]
-        synth_df.to_csv("/home/ubuntu/projects/myproject/pybo/synth_dir/" + obj + ".csv", index= False)
+        synth_df.to_csv("/home/ubuntu/FlaskProject/pybo/synth_dir/" + obj + ".csv", index= False)
         return render_template('synth_generate.html', df=synth_df2, dtypes=synth_df.dtypes)
         # return render_template('synth_generate.html', df=df, dtypes=dtypes)
 
@@ -218,11 +218,11 @@ def partsynth_generate():
         obj = g.user.username
 
         # df = pd.read_csv("C:/finalproject/myproject/pybo/uploads/" + obj + ".csv")
-        with open("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json", 'r') as f:
+        with open("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json", 'r') as f:
             dtypes = json.load(f)
         columns = list(dtypes.keys())
 
-        df = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv", header=None, skiprows = 1, names=columns).astype(dtypes)
+        df = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv", header=None, skiprows = 1, names=columns).astype(dtypes)
         # 헤더가 있는 경우 -> skip
         df.apply(pd.to_numeric, errors='coerce')
 
@@ -243,7 +243,7 @@ def partsynth_generate():
         result = pd.concat([df, synth_df2], axis=1)
 
         result2 = result.iloc[:10]
-        result.to_csv("/home/ubuntu/projects/myproject/pybo/synth_dir/" + obj + ".csv", index= False)
+        result.to_csv("/home/ubuntu/FlaskProject/pybo/synth_dir/" + obj + ".csv", index= False)
         return render_template('partsynth_generate.html', df=result2, dtypes=result2.dtypes)
 
 
@@ -252,9 +252,9 @@ def partsynth_generate():
 @bp.route('/distribution', methods=['GET', 'POST'])
 def distribution():
     obj = g.user.username
-    original_data = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+    original_data = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
     original_data = original_data.iloc[:,1:]
-    synth_data = pd.read_csv("/home/ubuntu/projects/myproject/pybo/synth_dir/" + obj + ".csv")
+    synth_data = pd.read_csv("/home/ubuntu/FlaskProject/pybo/synth_dir/" + obj + ".csv")
     cate_col = []
     for i in range(0, len(original_data.columns)):
         if original_data.dtypes[i] != 'object':
@@ -268,7 +268,7 @@ def distribution():
         plt.hist(original_data[cate_col[i]], alpha=0.3)
         plt.title(cate_col[i])
         plt.legend(['synth', 'origin'])
-        plt.savefig('/home/ubuntu/projects/myproject/pybo/static/img_dir/' + obj + 'dis.png')
+        plt.savefig('/home/ubuntu/FlaskProject/pybo/static/img_dir/' + obj + 'dis.png')
 
     plt.close()
     return render_template('distribution.html', image_file='/img_dir/'+ obj + 'dis.png')
@@ -278,20 +278,20 @@ def distribution():
 def correlation():
     obj = g.user.username
     plt.close()
-    original_data = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+    original_data = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
     original_data = original_data.iloc[:, 1:]
-    synth_data = pd.read_csv("/home/ubuntu/projects/myproject/pybo/synth_dir/" + obj + ".csv")
+    synth_data = pd.read_csv("/home/ubuntu/FlaskProject/pybo/synth_dir/" + obj + ".csv")
     corr_df = original_data.corr()
     corr_df = corr_df.apply(lambda x: round(x, 2))
 
     ax = sns.heatmap(corr_df, annot=True, annot_kws=dict(color='g'), cmap='Greys')
-    plt.savefig('/home/ubuntu/projects/myproject/pybo/static/img_dir/' + obj + 'origincorr.png')
+    plt.savefig('/home/ubuntu/FlaskProject/pybo/static/img_dir/' + obj + 'origincorr.png')
     plt.close()
     corr_df2 = synth_data.corr()
     corr_df2 = corr_df2.apply(lambda x: round(x, 2))
 
     ax2 = sns.heatmap(corr_df2, annot=True, annot_kws=dict(color='g'), cmap='Greys')
-    plt.savefig('/home/ubuntu/projects/myproject/pybo/static/img_dir/' + obj + 'synthcorr.png')
+    plt.savefig('/home/ubuntu/FlaskProject/pybo/static/img_dir/' + obj + 'synthcorr.png')
     plt.close()
 
     return render_template('correlation.html', synth_file='/img_dir/'+ obj + 'synthcorr.png', origin_file='/img_dir/'+ obj + 'origincorr.png')
@@ -300,11 +300,11 @@ def correlation():
 def hello_pybo3():
     obj = g.user.username
 
-    file = "/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv"
+    file = "/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv"
     if os.path.isfile(file):
         os.remove(file)
-    path = "/home/ubuntu/projects/myproject/pybo/synth_dir/" + obj + ".csv"
-    file2 = "/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".json"
+    path = "/home/ubuntu/FlaskProject/pybo/synth_dir/" + obj + ".csv"
+    file2 = "/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".json"
     if os.path.isfile(file2):
         os.remove(file2)
 
@@ -319,7 +319,7 @@ def hello_pybo():
 @bp.route('/hello2',  methods=['GET','POST'])
 def hello_pybo2():
     obj = g.user.username
-    df_info = pd.read_csv("/home/ubuntu/projects/myproject/pybo/uploads/" + obj + ".csv")
+    df_info = pd.read_csv("/home/ubuntu/FlaskProject/pybo/uploads/" + obj + ".csv")
     df_col = []
     list1 = []
     for i in range(0, len(df_info.columns)):
